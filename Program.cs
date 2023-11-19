1678c4fc2954b8c445060dfbcb66ce7ab3b06288
= <=>
2023-11-19 11:10:28 +0300

    ReviewAnalyzer/Готов

--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
-﻿ReviewAnalyzer.Start();
+﻿Console.InputEncoding = System.Text.Encoding.GetEncoding("utf-16");
+
+Console.OutputEncoding = System.Text.Encoding.GetEncoding("utf-16");
+
+ReviewAnalyzer.Start();
